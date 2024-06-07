e7ffe655187599fc25afbd7e9941e31098b8e71f
Nicolay <nicolaymorenoherrera@gmail.com>
2024-06-06 19:40:31 -0500

    app.test.ts: finiched example.

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,10 +1,24 @@
-
-
+import { ServerApp } from './presentation/server-app';
 
 describe('App', () => {
 
-    test('should run the test', () => {
-        expect(true).toBe(true);
+    test('should call Server.run with the correct parameters', async() => {
+
+        const mockServerRun = jest.fn();
+        ServerApp.run = mockServerRun;
+
+        process.argv = ['node', 'src/app.ts', '-b', '5', '-l', '10', '-s', '-n', 'test', '-d', 'test'];
+
+        await import('./app');
+
+        expect( mockServerRun ).toHaveBeenCalledWith({
+            base: 5,
+            limit: 10,
+            showTable: true,
+            fileName: 'test',
+            fileDestination: 'test'
+        })
+
     });
 
 });
